d8b2bb1e5550f5794bd673859bdd64bf3179a56b
miapoder <eaa25mpo@students.eaaa.dk>
2026-08-29 15:21:31 +0200

    Opdel CSS i separate filer

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import "./styles.css";
+import "./styles/global.css";
+import "./styles/components.css";
+import "./styles/pages.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
